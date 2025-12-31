7a7d747074b425150ca5f852d8cd89b03ad8522b
MonsefPRO <monsefelaidi.pro@gmail.com>
2025-12-31 09:34:59 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header({ onBlueBanner = false }: HeaderProps) {
   return (
     <header className="fixed w-full bg-white/98 backdrop-blur-md shadow-md z-40 transition-all border-b border-gray-100">
       <nav className="container mx-auto px-4 sm:px-8 lg:px-8">
-        <div className="flex justify-between items-center h-56">
+        <div className="flex justify-between items-center h-70">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0">
               <img
