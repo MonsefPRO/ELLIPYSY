554dbcc07753cb12fb47140c279ec543ebb21340
MonsefPRO <monsefelaidi.pro@gmail.com>
2026-01-02 13:04:45 +0100

    Updated LanguageSwitcher.tsx

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -28,19 +28,19 @@ export default function LanguageSwitcher() {
         aria-label="English"
         title="English"
       >
-        <svg viewBox="0 0 60 30" className="w-full h-full">
+        <svg viewBox="0 0 3 2" className="w-full h-full" preserveAspectRatio="none">
           <clipPath id="s">
-            <path d="M0,0 v30 h60 v-30 z"/>
+            <path d="M0,0 v2 h3 v-2 z"/>
           </clipPath>
           <clipPath id="t">
-            <path d="M30,15 h30 v15 z v-30 h-30 z h-30 v15 z v-30 h30 z"/>
+            <path d="M1.5,1 h1.5 v1 z v-2 h-1.5 z h-1.5 v1 z v-2 h1.5 z"/>
           </clipPath>
           <g clipPath="url(#s)">
-            <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
-            <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" strokeWidth="6"/>
-            <path d="M0,0 L60,30 M60,0 L0,30" clipPath="url(#t)" stroke="#C8102E" strokeWidth="4"/>
-            <path d="M30,0 v30 M0,15 h60" stroke="#fff" strokeWidth="10"/>
-            <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" strokeWidth="6"/>
+            <path d="M0,0 v2 h3 v-2 z" fill="#012169"/>
+            <path d="M0,0 L3,2 M3,0 L0,2" stroke="#fff" strokeWidth="0.3"/>
+            <path d="M0,0 L3,2 M3,0 L0,2" clipPath="url(#t)" stroke="#C8102E" strokeWidth="0.2"/>
+            <path d="M1.5,0 v2 M0,1 h3" stroke="#fff" strokeWidth="0.5"/>
+            <path d="M1.5,0 v2 M0,1 h3" stroke="#C8102E" strokeWidth="0.3"/>
           </g>
         </svg>
       </button>
